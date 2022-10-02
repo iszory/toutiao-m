9a6618d1cf7e520d59c5d3c406811809f6d68e8d
unknown <zoryduan@163.com>
2022-10-02 21:50:11 +0800

    项目初始化-调整初始代码结构

--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './styles/index.less'
 
+// eslint-disable-next-line eol-last
 createApp(App).use(store).use(router).mount('#app')
