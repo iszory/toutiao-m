8b126c246008df299b36f1106939e9194d8e0b2d
unknown <zoryduan@163.com>
2022-10-04 16:54:43 +0800

    项目初始化-引入vant组件库

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <template>
   <router-view />
-  <h1>gvvss</h1>
+  <h1>qgvvhpa</h1>
+  <div>
+    <i class="toutiao toutiao-pinglun"></i>
+  </div>
+  <van-button type="primary">主要按钮</van-button>
+  <van-button type="success">成功按钮</van-button>
+  <van-button type="default">默认按钮</van-button>
+  <van-button type="warning">警告按钮</van-button>
+  <van-button type="danger">危险按钮</van-button>
+  <van-cell-group>
+    <van-cell title="单元格" value="内容" />
+    <van-cell title="单元格" value="内容" label="描述信息" />
+  </van-cell-group>
 </template>
 
 <script>
@@ -9,6 +21,4 @@ export default {
 }
 </script>
 
-<style lang="less">
-
-</style>
+<style lang="less"></style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './styles/index.less'
+import Vant from 'vant'
+// import { Button } from 'vant'
+import 'vant/lib/index.css'
+// const app = createApp()
+// // 3. 注册你需要的组件
+// app.use(Button)
 
 // eslint-disable-next-line eol-last
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(Vant).mount('#app')
